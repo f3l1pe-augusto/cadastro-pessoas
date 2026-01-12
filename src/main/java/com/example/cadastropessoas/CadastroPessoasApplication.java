2f52c5325f06d9666c93a0efeed3861e9deb0e1d
felipe <fa.ferreira@unesp.br>
2026-01-12 19:16:19 -0300

    Fix Correção path aplicação

--- src/main/java/com/example/cadastropessoas/CadastroPessoasApplication.java
+++ src/main/java/com/example/cadastropessoas/CadastroPessoasApplication.java
@@ -1,4 +1,4 @@
-package com.example.cadastro_pessoas;
+package com.example.cadastropessoas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/example/cadastropessoas/CadastroPessoasApplicationTests.java
+++ src/test/java/com/example/cadastropessoas/CadastroPessoasApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.cadastro_pessoas;
+package com.example.cadastropessoas;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
