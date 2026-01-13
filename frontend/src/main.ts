d2894d1b2e533104a778258318776ecd198c4666
felipe <fa.ferreira@unesp.br>
2026-01-13 18:23:34 -0300

    Refactor Ajustes iniciais

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,12 +1,9 @@
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-
 import App from './App.vue'
 import router from './router'
 
-const app = createApp(App)
+import 'bootstrap/dist/css/bootstrap.min.css'
 
-app.use(createPinia())
-app.use(router)
-
-app.mount('#app')
+createApp(App)
+  .use(router)
+  .mount('#app')
--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,8 +1,34 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+import PessoaListView from '@/views/PessoaListView.vue'
+import PessoaFormView from "@/views/PessoaFormView.vue";
+
+const routes = [
+  {
+    path: '/',
+    redirect: '/pessoas'
+  },
+  {
+    path: '/pessoas',
+    name: 'pessoas',
+    component: PessoaListView
+  },
+  {
+    path: '/cadastrar',
+    name: 'cadastrar-pessoa',
+    component: PessoaFormView
+  },
+  {
+    path: '/editar/:id',
+    name: 'editar-pessoa',
+    component: PessoaFormView,
+    props: true
+  }
+]
+
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  history: createWebHistory(),
+  routes
 })
 
 export default router
