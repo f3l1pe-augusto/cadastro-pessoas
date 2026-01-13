ea3881fc85d4a1db4d8b0fd8025cc476f2fb02fd
felipe <fa.ferreira@unesp.br>
2026-01-12 21:13:59 -0300

    Add Adicionar atributos as interfaces

--- src/main/java/com/example/cadastropessoas/domain/pessoajuridica/entity/IPessoaJuridica.java
+++ src/main/java/com/example/cadastropessoas/domain/pessoajuridica/entity/IPessoaJuridica.java
@@ -1,6 +1,16 @@
 package com.example.cadastropessoas.domain.pessoajuridica.entity;
 
+import com.example.cadastropessoas.domain.pessoa.entity.IPessoa;
+
 public interface IPessoaJuridica {
+  /* Getters */
   String getCnpj();
   String getRazaoSocial();
+
+  /* Setters */
+  void setCnpj(String cnpj);
+  void setRazaoSocial(String razaoSocial);
+
+  /* Applies */
+  void applyPessoa(IPessoa pessoa);
 }
--- src/main/java/com/example/cadastropessoas/domain/telefone/entity/ITelefone.java
+++ src/main/java/com/example/cadastropessoas/domain/telefone/entity/ITelefone.java
@@ -1,9 +1,18 @@
 package com.example.cadastropessoas.domain.telefone.entity;
 
+import com.example.cadastropessoas.domain.pessoa.entity.IPessoa;
 import com.example.cadastropessoas.domain.tipotelefone.entity.TipoTelefone;
 
 public interface ITelefone {
+  /* Getters */
   Long getId();
   String getNumero();
   TipoTelefone getTipoTelefone();
+
+  /* Setters */
+  void setNumero(String numero);
+  void setTipoTelefone(TipoTelefone tipoTelefone);
+
+  /* Applies */
+  void applyPessoa(IPessoa pessoa);
 }
