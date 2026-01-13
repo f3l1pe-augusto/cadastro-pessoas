112346d020858844433b84f00a0a923145403519
felipe <fa.ferreira@unesp.br>
2026-01-13 10:59:56 -0300

    Refactor Simplificar DTO

--- src/main/java/com/example/cadastropessoas/infrastructure/repository/pessoa/dto/PessoaDTO.java
+++ src/main/java/com/example/cadastropessoas/infrastructure/repository/pessoa/dto/PessoaDTO.java
@@ -1,13 +1,9 @@
 package com.example.cadastropessoas.infrastructure.repository.pessoa.dto;
 
-import com.example.cadastropessoas.infrastructure.repository.endereco.dto.EnderecoDTO;
 import com.example.cadastropessoas.infrastructure.repository.pessoafisica.dto.PessoaFisicaDTO;
 import com.example.cadastropessoas.infrastructure.repository.pessoajuridica.dto.PessoaJuridicaDTO;
-import com.example.cadastropessoas.infrastructure.repository.telefone.dto.TelefoneDTO;
 import lombok.Getter;
 import lombok.Setter;
-
-import java.util.List;
 
 @Getter
 @Setter
@@ -17,6 +13,4 @@ public class PessoaDTO {
   private String email;
   private PessoaFisicaDTO pessoaFisica;
   private PessoaJuridicaDTO pessoaJuridica;
-  private List<EnderecoDTO> enderecos;
-  private List<TelefoneDTO> telefones;
 }
